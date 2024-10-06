8e666dbd447b08124339ce47509e3ddda7820fa0
shreya <shreyav0304@gmail.com>
2024-10-06 21:56:32 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a and b:\n");
+    scanf("%d %d",&a,&b);
+    
+    int sum=a+b;
+    printf("sum is %d\n",sum);
+    return 0;
+}
+//video stopped at 43:48
